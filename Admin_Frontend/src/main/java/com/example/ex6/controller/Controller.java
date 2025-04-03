bd858430a65b79332d226fb37bcbfacb15020adb
elif-mee <elif.kaya@studentfr.ch>
2025-04-03 14:10:37 +0200

    r

--- Admin_Frontend/src/main/java/com/example/ex6/controller/Controller.java
+++ Admin_Frontend/src/main/java/com/example/ex6/controller/Controller.java
@@ -9,7 +9,7 @@ import com.example.ex6.service.PaysService;
 import com.example.ex6.service.SkieurService;
 
 import java.util.ArrayList;
-
+//nigga
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
