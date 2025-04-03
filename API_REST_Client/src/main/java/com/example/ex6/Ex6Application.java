ce4b4c56c327d7dd286c6c27558586d5c6e020f4
MazenAlsawaf <imtired000111@gmail.com>
2025-04-03 16:40:40 +0200

    e

--- API_REST_Client/src/main/java/com/example/ex6/Ex6Application.java
+++ API_REST_Client/src/main/java/com/example/ex6/Ex6Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class app {
+public class Ex6Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(app.class, args);
+		SpringApplication.run(Ex6Application.class, args);
 	}
 
 }
